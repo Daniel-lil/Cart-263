83a256de987821dd07a0c4e89fd875294570d229
Daniel Little <Danswim02@gmail.com>
2022-02-11 02:21:45 -0500

    finish activity bubble pop

--- Activities/bubble-popper/js/script.js
+++ Activities/bubble-popper/js/script.js
@@ -8,6 +8,14 @@ author, and this description to match your project!
 
 "use strict";
 
+//stores the user's webcam
+let video = undefined;
+//stores the handpose model
+let handpose = undefined;
+//stores the current set of predictions
+let predictions = [];
+//stores bubbles
+let bubble = undefined;
 
 /**
 Description of preload
@@ -21,7 +29,33 @@ function preload() {
 Description of setup
 */
 function setup() {
+  createCanvas(640, 480);
 
+  //access user's webcam
+  video = createCapture(VIDEO);
+  video.hide();
+
+  //load the handpose model
+  handpose = ml5.handpose(video, {
+    flipHorizontal: true
+  }, function() {
+    console.log(`Model loaded.`);
+  });
+
+  //listen for predictions
+  handpose.on(`predict`, function (results) {
+    console.log(results);
+    predictions = results;
+  });
+
+  //bubble
+  bubble = {
+    x: random(width),
+    y: height,
+    size: 100,
+    vx: 0,
+    vy: -2
+  }
 }
 
 
@@ -29,5 +63,54 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  background(0);
 
+  if (predictions.length > 0) {
+    let hand = predictions[0];
+    let index = hand.annotations.indexFinger;
+    let tip = index[3];
+    let base = index[0];
+    let tipX = tip[0];
+    let tipY = tip[1];
+    let baseX = base[0];
+    let baseY = base[1];
+
+    push();
+    //pin body
+    noFill();
+    stroke(255);
+    strokeWeight(2);
+    line(baseX, baseY, tipX, tipY);
+    pop();
+
+    push();
+    //pin head
+    noStroke();
+    fill(255,0,0);
+    ellipse(baseX, baseY, 20);
+    pop();
+
+    //check bubble popping
+    let d = dist(tipX, tipY, bubble.x, bubble.y);
+    if (d < bubble.size / 2) {
+      bubble.x = random(width);
+      bubble.y = height;
+    }
+  }
+
+//move the bubble
+  bubble.x += bubble.vx;
+  bubble.y += bubble.vy;
+
+  if (bubble.y < 0) {
+    bubble.x = random(width);
+    bubble.y = height;
+  }
+
+//draw the bubble
+  push();
+  fill(0, 100, 200);
+  noStroke();
+  ellipse(bubble.x, bubble.y, bubble.size);
+  pop();
 }
